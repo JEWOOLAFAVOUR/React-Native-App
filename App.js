3af08b606378c9313ac47cc9e7732802f7d44dbd
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-15 14:51:58 -0800

    ksksks

--- App.js
+++ App.js
@@ -1,17 +1,50 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react';
-import Page1 from './page1';
-import Page2 from './page2';
+import React from 'react'
 
 const App = () => {
   return (
-    <View>
-      {/* <Page1 /> */}
-      <Page2 />
+    <View style={styles.page}>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        {/* FIRST BOX  */}
+        <View style={styles.box1}>
+
+        </View>
+        {/* SECOND BOX */}
+        <View style={styles.box2}>
+
+        </View>
+        {/* THIRD BOX */}
+        <View style={styles.box3}></View>
+
+      </View>
     </View>
   )
 }
 
-export default App;
+export default App
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 30,
+  },
+  box1: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'tomato',
+    marginBottom: 20,
+  },
+  box2: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'indigo',
+    marginBottom: 20,
+  },
+  box3: {
+    height: 80,
+    width: 80,
+    backgroundColor: 'purple',
+  },
+})
