5e4384f19a97ae38f58bec46a7176141e886c89c
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-16 13:15:18 -0800

    hfhfhfhfh

--- App.js
+++ App.js
@@ -3,48 +3,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <View style={styles.page}>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        {/* FIRST BOX  */}
-        <View style={styles.box1}>
-
-        </View>
-        {/* SECOND BOX */}
-        <View style={styles.box2}>
-
-        </View>
-        {/* THIRD BOX */}
-        <View style={styles.box3}></View>
-
-      </View>
+    <View>
+      <Text>App</Text>
     </View>
   )
 }
 
 export default App
 
-const styles = StyleSheet.create({
-  page: {
-    flex: 1,
-    backgroundColor: 'white',
-    padding: 30,
-  },
-  box1: {
-    height: 80,
-    width: 80,
-    backgroundColor: 'tomato',
-    marginBottom: 20,
-  },
-  box2: {
-    height: 80,
-    width: 80,
-    backgroundColor: 'indigo',
-    marginBottom: 20,
-  },
-  box3: {
-    height: 80,
-    width: 80,
-    backgroundColor: 'purple',
-  },
-})
+const styles = StyleSheet.create({})
