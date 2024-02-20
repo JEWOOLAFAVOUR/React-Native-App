ceb72288318ee1a1de7e2b814d25e4883fbd6c06
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-20 13:15:11 -0800

    sjsjsjs

--- App.js
+++ App.js
@@ -1,70 +1,22 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-const App = () => {
+export default function App() {
+
+  const name = ['Favour', 'Silvernus', 'Cephas', 120];
+
   return (
     <View style={styles.page}>
-      {/* BOXES  */}
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <TouchableOpacity  style={[styles.box1, { backgroundColor: 'green' }]}>
-        </TouchableOpacity>
-        <View style={[styles.box1, { backgroundColor: 'yellow' }]}></View>
-        <View style={styles.box1}></View>
-      </View>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-      </View>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-      </View>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-      </View>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-      </View>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-      </View>
-
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-        <View style={styles.box1}></View>
-      </View>
+      <Text>App</Text>
     </View>
-  )
-}
-
-export default App
+  );
+};
 
 const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: 50,
-    marginHorizontal: 20,
-  },
-  box1: {
-    height: 100,
-    width: 100,
-    backgroundColor: 'indigo',
-    marginBottom: 10,
-    borderRadius: 5
+    paddingTop: 30,
+    paddingLeft: 20,
   },
 })
---
+++ test.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+const studentName = ['Favor', 'Silvernus', 'Cephas', 120];
+console.log('Favour')
+console.log('Silvernus')
+console.log('Cephas')
+console.log(120)
+
+
+studentName.map((data, index) => {
+    console.log(data)
+    console.log(index)
+})
