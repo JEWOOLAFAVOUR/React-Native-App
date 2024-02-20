6e16cbb5285d8048cbbf560860fa9675b420668b
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-20 12:45:53 -0800

    tuesday first push

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const App = () => {
   return (
     <View style={styles.page}>
-      <Text>App</Text>
+      {/* BOXES  */}
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <TouchableOpacity  style={[styles.box1, { backgroundColor: 'green' }]}>
+        </TouchableOpacity>
+        <View style={[styles.box1, { backgroundColor: 'yellow' }]}></View>
+        <View style={styles.box1}></View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+      </View>
+
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 10 }}>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+        <View style={styles.box1}></View>
+      </View>
     </View>
   )
 }
@@ -15,6 +57,14 @@ const styles = StyleSheet.create({
   page: {
     flex: 1,
     backgroundColor: 'white',
-    paddingTop: 30,
-  }
+    paddingTop: 50,
+    marginHorizontal: 20,
+  },
+  box1: {
+    height: 100,
+    width: 100,
+    backgroundColor: 'indigo',
+    marginBottom: 10,
+    borderRadius: 5
+  },
 })
