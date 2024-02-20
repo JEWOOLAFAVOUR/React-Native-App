ab29bd3b09da942b291952430a48f6407ab3d8cc
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-20 14:19:32 -0800

    end of class for tuesday

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 
 const App = () => {
+  const Stack = createStackNavigator();
   return (
     <View>
       <Text>App</Text>
