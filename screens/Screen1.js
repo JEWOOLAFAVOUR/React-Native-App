f928d682ae60eb17d922970ef61ab224e4948f7e
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-21 15:01:30 -0800

    navigation dealings

--- screens/Screen1.js
+++ screens/Screen1.js
@@ -1,14 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const Screen1 = () => {
+
+const Screen1 = ({ navigation }) => {
+
     return (
-        <View>
+        <View style={styles.page}>
+            <Text style={{ fontSize: 20, }}>Welcome</Text>
             <Text>Screen1</Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Screen2')}
+                style={styles.button}>
+                <Text style={{ color: 'white' }}>Start</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 export default Screen1
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: 20,
+        paddingHorizontal: 20,
+    },
+    button: {
+        height: 30,
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+})
--- screens/Screen2.js
+++ screens/Screen2.js
@@ -1,14 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 import React from 'react'
 
-const Screen2 = () => {
+const Screen2 = ({ navigation }) => {
     return (
-        <View>
-            <Text>Screen2</Text>
+        <View style={styles.page}>
+            <Text>Hello from Mr Silverbys</Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('ThirdScreen')} style={styles.button}>
+                <Text>Log in</Text>
+            </TouchableOpacity>
         </View>
     )
 }
 
 export default Screen2
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    page: {
+        flex: 1,
+        backgroundColor: 'white',
+        padding: 20,
+    },
+    button: {
+        height: 40,
+        backgroundColor: 'blue',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+})
