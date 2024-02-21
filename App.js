828d2ed6710c93bd250e07668d7e5224f2e80291
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-02-21 14:41:43 -0800

    new pushsh

--- App.js
+++ App.js
@@ -2,13 +2,27 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
+import Screen1 from './screens/Screen1'
+import Screen2 from './screens/Screen2'
+import Screen3 from './screens/Screen3'
 
 const App = () => {
-  const Stack = createStackNavigator();
+  const Stack = createStackNavigator()
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+
+    <NavigationContainer>
+
+      <Stack.Navigator>
+
+        <Stack.Screen name='Screen1' component={Screen1} />
+        <Stack.Screen name='Screen2' component={Screen2} />
+        <Stack.Screen name='ThirdScreen' component={Screen3} />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
+
+
   )
 }
 
