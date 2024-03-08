d2e1562c96add73b71dc2d382012c0bda7e3cdaf
JEWOOLAFAVOUR <jewoolafavour2020@gmail.com>
2024-03-08 13:45:39 -0800

    when we're done

--- src/screens/Main/Home/HomeScreen.js
+++ src/screens/Main/Home/HomeScreen.js
@@ -3,14 +3,25 @@ import React, { useState } from 'react'
 
 const HomeScreen = () => {
 
-    const [data, setData] = useState('Favour')
+    // const [data, setData] = useState('Favour')
 
+    const [current, setCurrent] = useState(false)
+
+
+    const changeName = () => {
+
+    }
 
     return (
         <View style={styles.page}>
             <Text style={{ fontSize: 30, color: 'black', fontWeight: 'bold' }}>React Hooks Class - useState</Text>
 
-            <TouchableOpacity style={styles.button}>
+            <Text style={{
+                fontFamily: 20, color: 'black',
+                fontWeight: 'bold', marginTop: 30
+            }}>My name is {current ? "Silvernus" : "Favour"}</Text>
+
+            <TouchableOpacity onPress={() => setCurrent(!current)} style={styles.button}>
                 <Text style={{ fontSize: 15, color: '#fff', fontWeight: 'bold' }}>Change Text</Text>
             </TouchableOpacity>
         </View>
